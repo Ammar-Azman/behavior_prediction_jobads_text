d00863e051931cb948a0ea3af44ae9e0ba27ee35
Ammar-Azman <muhammarsafuan@gmail.com>
2024-01-15 17:08:48 +0800

    fature: error handling

--- main_ui.py
+++ main_ui.py
@@ -63,7 +63,6 @@ work_location = st.selectbox(
         ]
     ),
 )
-st.write(work_location)
 st.code("Click to predict user behavior!")
 button = st.button("Predict")
 
@@ -75,9 +74,21 @@ if button:
         st.error("No input in Location input.")
 
     else:
-        user_input = input_pipeline(jobdas_text_input, work_location)
-        model_preds = predict(user_input)
-        output, pred_probs = process_prediction(model_preds)
+        try:
+            user_input = input_pipeline(jobdas_text_input, work_location)
+        except Exception as e1:
+            print("ERROR e1: input_pipeline()")
+            print(f"ERROR e1:{e1}")
+        try:
+            model_preds = predict(user_input)
+        except Exception as e2:
+            print("ERROR e2: predict()")
+            print(f"ERROR e2:{e2}")
+        try:
+            output, pred_probs = process_prediction(model_preds)
+        except Exception as e3:
+            print("ERROR e3: process_prediction()")
+            print(f"ERROR e1:{e3}")
 
         if output == "V":
             st.caption("User predicted behavior: View")
